994c71d5ec6481dfa2942bae98e9ac5a66cad84f
Pavel Stoyanov <pstoyan@progress.com>
2025-12-17 15:30:38 +0200

    More meta information fixes and update the newtonsoft json

--- Sitefinity-VSIX.VS26/Properties/AssemblyInfo.cs
+++ Sitefinity-VSIX.VS26/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitefinity_VSIX.VS22")]
+[assembly: AssemblyTitle("Sitefinity_VSIX.VS26")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sitefinity_VSIX.VS22")]
+[assembly: AssemblyProduct("Sitefinity_VSIX.VS26")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
